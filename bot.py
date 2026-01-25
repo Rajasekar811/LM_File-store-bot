fd0a5d2af336ec918d20225784106a6331ef32a8
Rajasekar811 <111588149+Rajasekar811@users.noreply.github.com>
2026-01-25 08:38:33 +0530

    Add files via upload

--- bot.py
+++ bot.py
@@ -40,7 +40,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot Can't Export Invite link From Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double Check The FORCE_SUB_CHANNEL_1 Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_1}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/tech_freak_tamil for Support")
+                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/Lasera_Official for Support")
                 sys.exit()
 
         if FORCE_SUB_CHANNEL_2:
@@ -54,7 +54,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot can't Export Invite link from Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double check the FORCE_SUB_CHANNEL_2 value and Make sure Bot is Admin in channel with Invite Users via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_2}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/tech_freak_tamil for support")
+                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/Lasera_Official for support")
                 sys.exit()
         if FORCE_SUB_CHANNEL_3:
             try:
@@ -67,7 +67,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot Can't Export Invite link From Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double Check The FORCE_SUB_CHANNELS Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_3}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/tech_freak_tamil For Support")
+                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/Lasera_Oficial For Support")
                 sys.exit()
 
         if FORCE_SUB_CHANNEL_4:
@@ -81,7 +81,7 @@ class Bot(Client):
                 self.LOGGER(__name__).warning(a)
                 self.LOGGER(__name__).warning("Bot Can't Export Invite link From Force Sub Channel!")
                 self.LOGGER(__name__).warning(f"Please Double Check The FORCE_SUB_CHANNEL Value And Make Sure Bot Is Admin In Channel With Invite Users Via Link Permission, Current Force Sub Channel Value: {FORCE_SUB_CHANNEL_4}")
-                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/tech_freak_tamil For Support")
+                self.LOGGER(__name__).info("\nBot Stopped. https://t.me/Lasera_Oficial For Support")
                 sys.exit()
 
         try:
@@ -92,12 +92,12 @@ class Bot(Client):
         except Exception as e:
             self.LOGGER(__name__).warning(e)
             self.LOGGER(__name__).warning(f"Make Sure Bot Is Admin In DB Channel, And Double Check The CHANNEL_ID Value, Current Value: {CHANNEL_ID}")
-            self.LOGGER(__name__).info("\nBot Stopped. https://t.me/tech_freak_tamil For Support")
+            self.LOGGER(__name__).info("\nBot Stopped. https://t.me/Lasera_Official For Support")
             sys.exit()
 
         self.set_parse_mode(ParseMode.HTML)
-        self.LOGGER(__name__).info(f"Bot Running...!\n\nCreated By \nhttps://t.me/Tech_freak_tamil")
-        self.LOGGER(__name__).info(f"""ミ💖 TFT Developer 💖彡""")
+        self.LOGGER(__name__).info(f"Bot Running...!\n\nCreated By \nhttps://t.me/raja_sekar_811")
+        self.LOGGER(__name__).info(f"""ミ❤️‍🔥 Rajasekar ❤️‍🔥彡""")
         self.username = usr_bot_me.username
         #web-response
         app = web.AppRunner(await web_server())
@@ -114,7 +114,7 @@ class Bot(Client):
 
 
 
-# Tech freak 
+# Lasera_Oficial
 # Don't Remove Credit!!!
-# Telegram Channel @Tech_freak_tamil
-# Developer @devilo7
+# Telegram Channel @Lasera_Official
+# Developer @raja_sekar_811
