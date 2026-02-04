34694e901e7acae79eef13c4e98086eeec910de3
Rajasekar811 <111588149+Rajasekar811@users.noreply.github.com>
2026-02-04 09:59:47 +0530

    Add files via upload

--- config.py
+++ config.py
@@ -11,7 +11,7 @@ API_HASH = os.environ.get("API_HASH", "")
 
 
 OWNER_ID = int(os.environ.get("OWNER_ID", "5185252387","1416757994"))
-DB_URL = os.environ.get("DB_URL", "")
+DB_URL = os.environ.get("DB_URL", "mongodb+srv://sabarig491_db_user:QcOJqiQmA2VVCypq@cluster0.x5ocbtn.mongodb.net/?appName=Cluster0")
 DB_NAME = os.environ.get("DB_NAME", "Lasera_Official")
 
 
